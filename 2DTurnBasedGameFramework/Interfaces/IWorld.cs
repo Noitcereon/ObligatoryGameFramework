fab9d40118ca5644456d64574477346e338cbea8
Thomas (Noit) <tba@live.dk>
2021-03-15 10:32:11 +0100

    Add project files.

---
+++ 2DTurnBasedGameFramework/Interfaces/IWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DTurnBasedGameFramework.Interfaces
+{
+    public interface IWorld
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+    }
+}
