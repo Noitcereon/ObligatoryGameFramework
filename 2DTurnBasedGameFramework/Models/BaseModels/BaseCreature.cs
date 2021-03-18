1f1bf6347d67a88ca03757104a698c8235d823e2
Thomas (Noit) <tba@live.dk>
2021-03-18 15:38:49 +0100

    Added constructors and comments to them. Considering abstract vs non-abstract class.

--- 2DTurnBasedGameFramework/Models/BaseModels/BaseCreature.cs
+++ 2DTurnBasedGameFramework/Models/BaseModels/BaseCreature.cs
@@ -6,7 +6,7 @@ using Range = _2DTurnBasedGameFramework.Helpers.Range;
 
 namespace _2DTurnBasedGameFramework.Models.BaseModels
 {
-    public class BaseCreature : ICreature
+    public abstract class BaseCreature : ICreature
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
@@ -16,22 +16,77 @@ namespace _2DTurnBasedGameFramework.Models.BaseModels
         public Range Damage { get; set; }
         public int SpellPower { get; set; }
         public bool IsCaster { get; set; }
-        public Point Position { get; set; }
+        public Point Position { get; set; } = Point.Empty;
 
-        public BaseCreature() { }
+        // TODO: abstract the stats into a Stats class
+        // TODO: make SpellPower to be added with a Decorator if IsCaster = true
 
-        public BaseCreature(string name, int attack, int defense, int hitpoints, Range damage, Point position)
+        protected BaseCreature() { }
+
+        /// <summary>
+        /// The base constructor for a non-caster creature.
+        /// </summary>
+        /// <param name="name">Name of the creature type.</param>
+        /// <param name="attack">The attack bonus of the creature.</param>
+        /// <param name="defense">The defense bonus of the creature.</param>
+        /// <param name="hitpoints">Total HP for the creature.</param>
+        /// <param name="damage">The damage range for the creature. (can go over the limit with the Attack bonus)</param>
+        protected BaseCreature(string name, int attack, int defense, int hitpoints, Range damage)
         {
-            // TODO: abstract the stats into a Stats class
-            // TODO: make SpellPower to be added with a Decorator if IsCaster = true
             Name = name;
-            Damage = damage;
             Attack = attack;
             Defense = defense;
             Hitpoints = hitpoints;
+            Damage = damage;
+        }
+
+        /// <summary>
+        /// The base constructor for a non-caster creature with an initial position set.
+        /// </summary>
+        /// <param name="name">Name of the creature type.</param>
+        /// <param name="attack">The attack bonus of the creature.</param>
+        /// <param name="defense">The defense bonus of the creature.</param>
+        /// <param name="hitpoints">Total HP for the creature.</param>
+        /// <param name="damage">The damage range for the creature. (can go over the limit with the Attack bonus)</param>
+        /// <param name="position">The initial position of the creature in a <c>World</c></param>
+        protected BaseCreature(string name, int attack, int defense, int hitpoints, Range damage, Point position):this
+            (name, attack, defense, hitpoints, damage)
+        {
             Position = position;
-            SpellPower = 0;
-            IsCaster = false;
+        }
+
+        /// <summary>
+        /// The base constructor for a caster creature.
+        /// </summary>
+        /// <param name="name">Name of the creature type.</param>
+        /// <param name="attack">The attack bonus of the creature.</param>
+        /// <param name="defense">The defense bonus of the creature.</param>
+        /// <param name="hitpoints">Total HP for the creature.</param>
+        /// <param name="damage">The damage range for the creature. (can go over the limit with the Attack bonus)</param>
+        /// <param name="isCaster">Determines wheter or not the creature is a caster.</param>
+        protected BaseCreature(string name, int attack, int defense, int hitpoints, Range damage, bool isCaster) : this
+            (name, attack, defense, hitpoints, damage)
+        {
+            IsCaster = isCaster;
+            if (IsCaster) SpellPower = 10;
+        }
+
+        /// <summary>
+        /// The base constructor for a caster creature with an initial position set.
+        /// </summary>
+        /// <param name="name">Name of the creature type.</param>
+        /// <param name="attack">The attack bonus of the creature.</param>
+        /// <param name="defense">The defense bonus of the creature.</param>
+        /// <param name="hitpoints">Total HP for the creature.</param>
+        /// <param name="damage">The damage range for the creature. (can go over the limit with the Attack bonus)</param>
+        /// <param name="position">The initial position of the creature in a <c>World</c></param>
+        /// <param name="isCaster">Determines wheter or not the creature is a caster.</param>
+        protected BaseCreature(string name, int attack, int defense, int hitpoints, Range damage, Point position, bool isCaster) : this
+            (name, attack, defense, hitpoints, damage)
+        {
+            IsCaster = isCaster;
+            if (IsCaster) SpellPower = 10;
+            Position = position;
         }
 
         public void InteractWithWorldObject()
@@ -42,8 +97,10 @@ namespace _2DTurnBasedGameFramework.Models.BaseModels
         public virtual int Hit()
         {
             Random random = new Random();
-
+            
             int baseDamage = random.Next(Damage.From, Damage.To);
+
+            // abstract dmg calculation into derived class?
             int damageDealt = baseDamage + Attack;
 
             return damageDealt;
@@ -51,6 +108,7 @@ namespace _2DTurnBasedGameFramework.Models.BaseModels
 
         public virtual int ReceiveHit(int damage)
         {
+            // Maybe abstract the damage calculation away into a method overriden in a derived class?
             int damageTaken = damage - Defense;
 
             Hitpoints -= damageTaken;
---
+++ 2DTurnBasedGameFramework/Models/GenericCreature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using _2DTurnBasedGameFramework.Models.BaseModels;
+using Range = _2DTurnBasedGameFramework.Helpers.Range;
+
+namespace _2DTurnBasedGameFramework.Models
+{
+    public class GenericCreature : BaseCreature
+    {
+        public GenericCreature()
+        {
+        }
+
+        public GenericCreature(string name, int attack, int defense, int hitpoints, Range damage) : base(name, attack, defense, hitpoints, damage)
+        {
+        }
+
+        public GenericCreature(string name, int attack, int defense, int hitpoints, Range damage, Point position) : base(name, attack, defense, hitpoints, damage, position)
+        {
+        }
+
+        public GenericCreature(string name, int attack, int defense, int hitpoints, Range damage, bool isCaster) : base(name, attack, defense, hitpoints, damage, isCaster)
+        {
+        }
+
+        public GenericCreature(string name, int attack, int defense, int hitpoints, Range damage, Point position, bool isCaster) : base(name, attack, defense, hitpoints, damage, position, isCaster)
+        {
+        }
+    }
+}
