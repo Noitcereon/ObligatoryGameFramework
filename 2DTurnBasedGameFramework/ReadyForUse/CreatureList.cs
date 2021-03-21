4dacf3b00f562a9b344ec7e5dd9202a470aaa34c
Thomas (Noit) <tba@live.dk>
2021-03-21 15:48:55 +0100

    Added TODO comment.

---
+++ 2DTurnBasedGameFramework/ReadyForUse/CreatureList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DTurnBasedGameFramework.ReadyForUse
+{
+    public class CreatureList
+    {
+        // TODO: make Ready-For-Use creatures and items (in the ReadyForUse folder), for someone to set up a game fast.
+    }
+}
